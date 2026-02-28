e980ce90fb365934a56e97ade857d0a1c8836636
OmkarMadchetti5758 <omkarmadchetti@gmail.com>
2026-02-28 21:29:43 +0530

    added health api

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import express from 'express';
-import 'dotenv/config';
-import contactRoutes from './routes/contact.routes';
+import express from "express";
+import "dotenv/config";
+import contactRoutes from "./routes/contact.routes";
 
 const app = express();
 
 app.use(express.json());
-app.use('/', contactRoutes);
+app.use("/api", contactRoutes);
 
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
